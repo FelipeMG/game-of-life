b53308e8ba35ab3b45ac4c9966fa0a22ecfd1c6a
FelipeMG <felipemontana@gmail.com>
2018-03-17 15:24:37 -0500

    added Cell enum and Builder inner class for the World

---
+++ src/main/java/Cell.java
@@ -0,0 +1,19 @@
+public enum Cell {
+    ALIVE {
+        public Cell next(int aliveNeighbors) {
+            if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+                return DEAD;
+            }
+            return this;
+        }
+    },
+    DEAD {
+        public Cell next(int aliveNeighbors) {
+            if (aliveNeighbors == 3) {
+                return ALIVE;
+            }
+            return this;
+        }
+    };
+    public abstract Cell next(int aliveNeighbors);
+}
